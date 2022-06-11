ef77d2ffc42841390902cd8faee7f6f8a7b0e303
Chinlying <zhouence518@126.com>
2022-06-11 12:14:44 +0800

    fix some bugs

--- project-layout/services/hello/README.md
+++ project-layout/services/hello/README.md
@@ -5,7 +5,7 @@ protoc --go_out=. --go-grpc_out=. proto/hello.proto
 ```
 or
 ```shell
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/hello.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/proto.proto
 ```
 
 ## Build and Run:
--- project-layout/services/hello/server/server.go
+++ project-layout/services/hello/server/server.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	"growing-into-an-excellent-golang-developer/project-layout/services/hello/proto"
 	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
@@ -14,14 +15,14 @@ type server struct {
 }
 
 func (s *server) SayHello(ctx context.Context, req *proto.SayRequest) (*proto.SayResponse, error) {
-	fmt.Println("request:", req.Name)
+	fmt.Printf("request:%v", req.Name)
 	return &proto.SayResponse{Message: "Hello " + req.Name}, nil
 }
 
 func main() {
 	listen, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		fmt.Println("failed to listen: %v", err)
+		fmt.Printf("failed to listen: %v", err)
 		return
 	}
 
@@ -37,7 +38,7 @@ func main() {
 	fmt.Println("Serving 50051...")
 	err = s.Serve(listen)
 	if err != nil {
-		fmt.Println("failed to serve: %v", err)
+		fmt.Printf("failed to serve: %v", err)
 		return
 	}
 
